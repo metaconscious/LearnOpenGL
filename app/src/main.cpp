f96a2525a1025c9d81e21aa00f76d75f22297787
Mark Oven <justmarkoven@gmail.com>
2025-05-13 11:12:25 +0800

    Initialize GLFW and GLAD in main function

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -1,8 +1,32 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+#include <print>
+
 int main(const int argc, char* argv[])
 {
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+#ifdef __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+
+    auto* window{ glfwCreateWindow(800, 600, "LearnOpenGL", nullptr, nullptr) };
+    if (window == nullptr)
+    {
+        std::println(stderr, "Failed to create GLFW window");
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    if (!gladLoadGLLoader(reinterpret_cast<GLADloadproc>(glfwGetProcAddress)))
+    {
+        std::println(stderr, "Failed to initialize GLAD");
+        return -1;
+    }
 
     return 0;
 }
