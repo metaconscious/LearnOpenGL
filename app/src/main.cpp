7e6795efab1a6fd78b612b1cd2108a9ce7c76420
Mark Oven <justmarkoven@gmail.com>
2025-05-15 15:27:10 +0800

    Support multiple VAOs and VBOs for rendering triangles

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -141,20 +141,26 @@ int main(const int argc, char* argv[])
     glDeleteShader(vertexShader);
     glDeleteShader(fragmentShader);
 
-    GLuint vertexArrayObject{};
-    glGenVertexArrays(1, &vertexArrayObject);
+    GLuint vertexArrayObjects[2]{};
+    glGenVertexArrays(2, vertexArrayObjects);
 
-    GLuint vertexBufferObject{};
-    glGenBuffers(1, &vertexBufferObject);
+    GLuint vertexBufferObjects[2]{};
+    glGenBuffers(2, vertexBufferObjects);
 
-    glBindVertexArray(vertexArrayObject);
+    glBindVertexArray(vertexArrayObjects[0]);
 
-    glBindBuffer(GL_ARRAY_BUFFER, vertexBufferObject);
+    glBindBuffer(GL_ARRAY_BUFFER, vertexBufferObjects[0]);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-    glUseProgram(shaderProgram);;
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(decltype(*vertices)), nullptr);
+    glEnableVertexAttribArray(0);
 
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), nullptr);
+    glBindVertexArray(vertexArrayObjects[1]);
+
+    glBindBuffer(GL_ARRAY_BUFFER, vertexBufferObjects[1]);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(decltype(*vertices)), nullptr);
     glEnableVertexAttribArray(0);
 
     glBindBuffer(GL_ARRAY_BUFFER, 0);
@@ -169,7 +175,10 @@ int main(const int argc, char* argv[])
         glClear(GL_COLOR_BUFFER_BIT);
 
         glUseProgram(shaderProgram);
-        glBindVertexArray(vertexArrayObject);
+        glBindVertexArray(vertexArrayObjects[0]);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+
+        glBindVertexArray(vertexArrayObjects[1]);
         glDrawArrays(GL_TRIANGLES, 0, 3);
 
         // Note: double buffer is used by default for modern OpenGL
@@ -177,8 +186,8 @@ int main(const int argc, char* argv[])
         glfwPollEvents(); // Processes the event queue and invoke appropriate callbacks
     }
 
-    glDeleteVertexArrays(1, &vertexArrayObject);
-    glDeleteBuffers(1, &vertexBufferObject);
+    glDeleteVertexArrays(2, vertexArrayObjects);
+    glDeleteBuffers(2, vertexBufferObjects);
     glDeleteProgram(shaderProgram);
 
     glfwTerminate();
