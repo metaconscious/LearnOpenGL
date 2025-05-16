6b52af2387fe0f35c9a0dbdc28b97ffe3fb57545
Mark Oven <justmarkoven@gmail.com>
2025-05-16 16:47:32 +0800

    Modify vertex shader to set color based on position coordinates

--- app/shaders/vertex.glsl
+++ app/shaders/vertex.glsl
@@ -1,7 +1,7 @@
 #version 330
 
 layout (location = 0) in vec3 aPos;
-layout (location = 1) in vec3 aColor;
+layout (location = 1) in vec3 aColor;// Unused
 
 uniform vec3 aOffset;
 
@@ -9,5 +9,5 @@ out vec3 ourColor;
 
 void main() {
     gl_Position = vec4(aPos.x + aOffset.x, -aPos.y + aOffset.y, aPos.z + aOffset.z, 1.0);
-    ourColor = aColor;
+    ourColor = gl_Position.xyz;
 }
