3c3310a61d9fac9ebb1d2d89e46bbb6473b64c29
Mark Oven <justmarkoven@gmail.com>
2025-05-20 16:01:27 +0800

    Adjust texture coordinates in main.cpp and modify texture mapping in fragment.glsl

--- app/shaders/fragment.glsl
+++ app/shaders/fragment.glsl
@@ -9,5 +9,5 @@ uniform sampler2D texture0;
 uniform sampler2D texture1;
 
 void main() {
-    FragColor = mix(texture(texture0, TexCoord), texture(texture1, TexCoord), 0.2);
+    FragColor = mix(texture(texture0, TexCoord), texture(texture1, TexCoord * 2), 0.2);
 }
