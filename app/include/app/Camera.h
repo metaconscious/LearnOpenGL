21e8b520fddf5baf7e543d4c654c91b427f9f07a
Mark Oven <justmarkoven@gmail.com>
2025-05-23 16:45:08 +0800

    Add Camera class skeleton and update CMakeLists.txt for automatic source file configuration

---
+++ app/include/app/Camera.h
@@ -0,0 +1,15 @@
+//
+// Created by user on 5/23/25.
+//
+
+#ifndef LEARNOPENGL_APP_CAMERA_H
+#define LEARNOPENGL_APP_CAMERA_H
+
+namespace lgl
+{
+    class Camera
+    {
+    };
+} // lgl
+
+#endif //LEARNOPENGL_APP_CAMERA_H
---
+++ app/src/Camera.cpp
@@ -0,0 +1,9 @@
+//
+// Created by user on 5/23/25.
+//
+
+#include "app/Camera.h"
+
+namespace lgl
+{
+} // lgl
