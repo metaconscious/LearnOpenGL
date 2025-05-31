eb1ce2d3e1a7ccadb9493a2dc7acc1171d09bed5
Mark Oven <justmarkoven@gmail.com>
2025-05-31 13:51:22 +0800

    Define light property structs and use them for lighting uniforms and colors

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -16,6 +16,45 @@
 constexpr auto DEFAULT_WINDOW_WIDTH{ 800 };
 constexpr auto DEFAULT_WINDOW_HEIGHT{ 600 };
 
+struct DirectionalLight
+{
+    glm::vec3 direction;
+
+    glm::vec3 ambient;
+    glm::vec3 diffuse;
+    glm::vec3 specular;
+};
+
+struct PointLight
+{
+    glm::vec3 position;
+
+    glm::vec3 color;
+    float ambient;
+    float diffuse;
+    float specular;
+
+    float constant;
+    float linear;
+    float quadratic;
+};
+
+struct SpotLight
+{
+    glm::vec3 position;
+    glm::vec3 direction;
+    float cutoff;
+    float outerCutoff;
+
+    glm::vec3 ambient;
+    glm::vec3 diffuse;
+    glm::vec3 specular;
+
+    float constant;
+    float linear;
+    float quadratic;
+};
+
 float vertices[]{
     // positions         // normals        / texture coords
     -0.5f, -0.5f, -0.5f, 0.0f, 0.0f, -1.0f, 0.0f, 0.0f,
@@ -74,11 +113,54 @@ glm::vec3 cubePositions[]{
     { -1.3f, 1.0f, -1.5f }
 };
 
-glm::vec3 pointLightPositions[]{
-    { 0.7f, 0.2f, 2.0f },
-    { 2.3f, -3.3f, -4.0f },
-    { -4.0f, 2.0f, -12.0f },
-    { 0.0f, 0.0f, -3.0f }
+DirectionalLight directionalLight{
+    .direction = { -0.2f, -1.0f, -0.3f },
+    .ambient = { 0.3f, 0.24f, 0.14f },
+    .diffuse = { 0.7f, 0.42f, 0.26f },
+    .specular = { 0.5f, 0.5f, 0.5f },
+};
+
+PointLight pointLights[]{
+    {
+        .position = { 0.7f, 0.2f, 2.0f },
+        .color = { 1.0f, 0.6f, 0.0f },
+        .ambient = 0.1f,
+        .diffuse = 1.0f,
+        .specular = 1.0f,
+        .constant = 1.0f,
+        .linear = 0.09f,
+        .quadratic = 0.032f
+    },
+    {
+        .position = { 2.3f, -3.3f, -4.0f },
+        .color = { 1.0f, 0.0f, 0.0f },
+        .ambient = 0.1f,
+        .diffuse = 1.0f,
+        .specular = 1.0f,
+        .constant = 1.0f,
+        .linear = 0.09f,
+        .quadratic = 0.032f
+    },
+    {
+        .position = { -4.0f, 2.0f, -12.0f },
+        .color = { 1.0f, 1.0, 0.0 },
+        .ambient = 0.1f,
+        .diffuse = 1.0f,
+        .specular = 1.0f,
+        .constant = 1.0f,
+        .linear = 0.09f,
+        .quadratic = 0.032f
+    },
+    {
+        .position = { 0.0f, 0.0f, -3.0f },
+        .color = { 0.2f, 0.2f, 1.0f },
+        .ambient = 0.1f,
+        .diffuse = 1.0f,
+        .specular = 1.0f,
+        .constant = 1.0f,
+        .linear = 0.09f,
+        .quadratic = 0.032f
+    },
 };
 
 void processInput(GLFWwindow* window)
@@ -277,37 +359,53 @@ int main(const int argc, char* argv[])
 
         cameraSystem.update(timeManager.getDeltaTime());
 
+        const SpotLight spotlight{
+            .position = camera->getPosition(),
+            .direction = camera->getForwardVector(),
+            .cutoff = glm::cos(glm::radians(12.5f)),
+            .outerCutoff = glm::cos(glm::radians(13.0f)),
+            .ambient = { 0.0f, 0.0f, 0.0f },
+            .diffuse = { 0.8f, 0.8f, 0.0f },
+            .specular = { 0.8f, 0.8f, 0.0f },
+            .constant = 1.0f,
+            .linear = 0.09f,
+            .quadratic = 0.032f
+        };
+
         processInput(window);
 
-        glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+        glClearColor(0.75f, 0.52f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         lightingShaderProgram.use();
         lightingShaderProgram.setUniform("material.shininess", 64.0f);
-        lightingShaderProgram.setUniform("directionalLight.direction", -0.2f, -1.0f, -0.3f);
-        lightingShaderProgram.setUniform("directionalLight.ambient", glm::vec3{ 0.05f });
-        lightingShaderProgram.setUniform("directionalLight.diffuse", glm::vec3{ 0.4f });
-        lightingShaderProgram.setUniform("directionalLight.specular", glm::vec3{ 0.5 });
-        for (auto&& [index, position] : std::views::enumerate(pointLightPositions))
+        lightingShaderProgram.setUniform("directionalLight.direction", directionalLight.direction);
+        lightingShaderProgram.setUniform("directionalLight.ambient", directionalLight.ambient);
+        lightingShaderProgram.setUniform("directionalLight.diffuse", directionalLight.diffuse);
+        lightingShaderProgram.setUniform("directionalLight.specular", directionalLight.specular);
+        for (auto&& [index, props] : std::views::enumerate(pointLights))
         {
-            lightingShaderProgram.setUniform(std::format("pointLights[{}].position", index), position);
-            lightingShaderProgram.setUniform(std::format("pointLights[{}].ambient", index), glm::vec3{ 0.05 });
-            lightingShaderProgram.setUniform(std::format("pointLights[{}].diffuse", index), glm::vec3{ 0.8f });
-            lightingShaderProgram.setUniform(std::format("pointLights[{}].specular", index), glm::vec3{ 1.0f });
-            lightingShaderProgram.setUniform(std::format("pointLights[{}].constant", index), 1.0f);
-            lightingShaderProgram.setUniform(std::format("pointLights[{}].linear", index), 0.09f);
-            lightingShaderProgram.setUniform(std::format("pointLights[{}].quadratic", index), 0.032f);
+            lightingShaderProgram.setUniform(std::format("pointLights[{}].position", index), props.position);
+            lightingShaderProgram.setUniform(std::format("pointLights[{}].ambient", index),
+                                             props.ambient * props.color);
+            lightingShaderProgram.setUniform(std::format("pointLights[{}].diffuse", index),
+                                             props.diffuse * props.color);
+            lightingShaderProgram.setUniform(std::format("pointLights[{}].specular", index),
+                                             props.specular * props.color);
+            lightingShaderProgram.setUniform(std::format("pointLights[{}].constant", index), props.constant);
+            lightingShaderProgram.setUniform(std::format("pointLights[{}].linear", index), props.linear);
+            lightingShaderProgram.setUniform(std::format("pointLights[{}].quadratic", index), props.quadratic);
         }
-        lightingShaderProgram.setUniform("spotLight.position", camera->getPosition());
-        lightingShaderProgram.setUniform("spotLight.direction", camera->getForwardVector());
-        lightingShaderProgram.setUniform("spotLight.cutoff", glm::cos(glm::radians(12.5f)));
-        lightingShaderProgram.setUniform("spotLight.outerCutoff", glm::cos(glm::radians(15.0f)));
-        lightingShaderProgram.setUniform("spotLight.ambient", glm::vec3{ 0.0f });
-        lightingShaderProgram.setUniform("spotLight.diffuse", glm::vec3{ 1.0f });
-        lightingShaderProgram.setUniform("spotLight.specular", glm::vec3{ 1.0f });
-        lightingShaderProgram.setUniform("spotLight.constant", 1.0f);
-        lightingShaderProgram.setUniform("spotLight.linear", 0.09f);
-        lightingShaderProgram.setUniform("spotLight.quadratic", 0.032f);
+        lightingShaderProgram.setUniform("spotLight.position", spotlight.position);
+        lightingShaderProgram.setUniform("spotLight.direction", spotlight.direction);
+        lightingShaderProgram.setUniform("spotLight.cutoff", spotlight.cutoff);
+        lightingShaderProgram.setUniform("spotLight.outerCutoff", spotlight.outerCutoff);
+        lightingShaderProgram.setUniform("spotLight.ambient", spotlight.ambient);
+        lightingShaderProgram.setUniform("spotLight.diffuse", spotlight.diffuse);
+        lightingShaderProgram.setUniform("spotLight.specular", spotlight.specular);
+        lightingShaderProgram.setUniform("spotLight.constant", spotlight.constant);
+        lightingShaderProgram.setUniform("spotLight.linear", spotlight.linear);
+        lightingShaderProgram.setUniform("spotLight.quadratic", spotlight.quadratic);
         lightingShaderProgram.setUniform("viewPos", camera->getPosition());
 
         const auto viewMatrix{ camera->getViewMatrix() };
@@ -348,15 +446,15 @@ int main(const int argc, char* argv[])
         }
 
         lightSourceShaderProgram.use();
-        lightSourceShaderProgram.setUniform("lightColor", glm::vec3{ 1.0 });
         glBindVertexArray(lightSourceVertexArrayObject);
-        for (auto&& position : pointLightPositions)
+        for (auto&& light : pointLights)
         {
+            lightSourceShaderProgram.setUniform("lightColor", light.color);
             const auto lightSourceModelMatrix{
                 glm::scale(
                     glm::translate(
                         glm::mat4{ 1.0f },
-                        position
+                        light.position
                     ),
                     glm::vec3{ 0.2 }
                 )
